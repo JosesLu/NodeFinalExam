6faa5888d37f2504edac441785155cc7ea6f715d
Joses Lu <Joses Lu>
2016-11-20 20:23:05 +0800

    Added landing page and Authentication through Session

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,97 +1,95 @@
 var email = [];
 var password = [];
+var currentUser = "";
+
 function passwordMismatchError() {
-	document.getElementById("modal-title").textContent="Error";
-	document.getElementById("modal-text").textContent="Passwords do not match";		
+	document.getElementById("modal-title").textContent = "Error";
+	document.getElementById("modal-text").textContent = "Passwords do not match";
 	$('#modal').modal('show');
-		$('#regEmail').val('');
-		$('#regPass').val('');
-		$('#regConfPass').val('');
+	$('#regEmail').val('');
+	$('#regPass').val('');
+	$('#regConfPass').val('');
 }
+
 function emailError() {
-	document.getElementById("modal-title").textContent="Error";
-	document.getElementById("modal-text").textContent="Email must contain atleast 3 characters.";		
+	document.getElementById("modal-title").textContent = "Error";
+	document.getElementById("modal-text").textContent = "Email must contain atleast 3 characters.";
 	$('#modal').modal('show');
-		$('#regEmail').val('');
-		$('#regPass').val('');
-		$('#regConfPass').val('');
+	$('#regEmail').val('');
+	$('#regPass').val('');
+	$('#regConfPass').val('');
 }
+
 function passwordLengthError() {
-	document.getElementById("modal-title").textContent="Error";
-	document.getElementById("modal-text").textContent="Passwords must contain atleast 8 characters.";		
+	document.getElementById("modal-title").textContent = "Error";
+	document.getElementById("modal-text").textContent = "Passwords must contain atleast 8 characters.";
 	$('#modal').modal('show');
-		$('#regEmail').val('');
-		$('#regPass').val('');
-		$('#regConfPass').val('');
+	$('#regEmail').val('');
+	$('#regPass').val('');
+	$('#regConfPass').val('');
 }
+
 function emptyFieldsError() {
-	document.getElementById("modal-title").textContent="Error";
-	document.getElementById("modal-text").textContent="All fields require input.";		
+	document.getElementById("modal-title").textContent = "Error";
+	document.getElementById("modal-text").textContent = "All fields require input.";
 	$('#modal').modal('show');
-		$('#regEmail').val('');
-		$('#regPass').val('');
-		$('#regConfPass').val('');
+	$('#regEmail').val('');
+	$('#regPass').val('');
+	$('#regConfPass').val('');
 }
-function register() {	
+
+function register() {
 	var counter = 0;
 	var regEmail = document.getElementById('regEmail').value;
 	var regPass = document.getElementById('regPass').value;
 	var regConfPass = document.getElementById('regConfPass').value;
-	$.ajax
-	    ({
-	        type: "POST",
-	        url: 'http://localhost:3000/users/',
-	        data: { 
-	        	"username" : regEmail,
-	        	"password" : regPass
-	        	
-	        	},
-	        success: function (){
-	        	console.log("Added Successfully");
-	        },
-	        
-	    });
-	/////
+	$.ajax({
+		type: "POST",
+		url: 'http://localhost:3000/users/',
+		data: {
+			"username": regEmail,
+			"password": regPass
 
+		},
+		success: function() {
+			console.log("Added Successfully");
+		},
 
-
-	if(regPass != regConfPass)
-	{
+	});
+	if (regPass != regConfPass) {
 		passwordMismatchError();
 		counter = 1;
 	}
-	if(regPass.length < 8)
-	{
+	if (regPass.length < 8) {
 		passwordLengthError();
 		counter = 1;
 	}
-	if(regEmail.length < 3)
-	{
+	if (regEmail.length < 3) {
 		emailError();
 		counter = 1;
 	}
-	if(regEmail.length < 1 && regPass < 1 && regConfPass < 1 )
-	{
+	if (regEmail.length < 1 && regPass < 1 && regConfPass < 1) {
 		emptyFieldsError();
 		counter = 1;
 	}
-	if(counter==0)
-	{
-		document.getElementById("modal-title").textContent="Success";
-		document.getElementById("modal-text").textContent="Registration Complete";
-	
+	if (counter == 0) {
+		document.getElementById("modal-title").textContent = "Success";
+		document.getElementById("modal-text").textContent = "Registration Complete";
+
 		$('#modal').modal('show');
 		$('#regEmail').val('');
 		$('#regPass').val('');
 		$('#regConfPass').val('');
-		$('#modal').on('hidden.bs.modal', function () {
+		$('#modal').on('hidden.bs.modal', function() {
 			window.location = '/login';
 		})
 	}
-	counter=0;
+	counter = 0;
 
 
 };
+
+
 function login() {
 
 	var counter = 0;
@@ -106,10 +104,14 @@ function login() {
 				document.getElementById("modal-title").textContent = "Success";
 				document.getElementById("modal-text").textContent = "Login Successful";
 				$('#modal').modal('show');
+				currentUser = logEmail;
 				$('#logEmail').val('');
 				$('#logPass').val('');
 				counter++;
+				sessionStorage.setItem('achoc', 'nein');
+				sessionStorage.setItem('user', currentUser);
 				window.location = '/dashboard';
+
 			}
 
 		}
@@ -127,33 +129,36 @@ function login() {
 	counter = 0;
 
 };
+
 function logout() {
+	sessionStorage.removeItem('achoc');
+	sessionStorage.removeItem('user');
 	document.getElementById("modal-title").textContent = "Success";
 	document.getElementById("modal-text").textContent = "You have successfuly logged out.";
 	$('#modal').modal('show');
 	$('#regEmail').val('');
 	$('#regPass').val('');
 	$('#regConfPass').val('');
-	$('#modal').on('hidden.bs.modal', function () {
+	$('#modal').on('hidden.bs.modal', function() {
 		window.location = '/';
 	})
 
 }
+
 function indextologin() {
 	window.location = '/login';
 }
 
 
 function rememberMe() {
-	  if (localStorage.chkbx && localStorage.chkbx != '') {
-                    $('#remember_me').attr('checked', 'checked');
-                    $('#logEmail').val(localStorage.usrname);
-                    $('#logPass').val(localStorage.pass);
-                } else {
-                    $('#remember_me').removeAttr('checked');
-                    $('#logEmail').val('');
-                    $('#logPass').val('');
-                }
- 
+	if (localStorage.chkbx && localStorage.chkbx != '') {
+		$('#remember_me').attr('checked', 'checked');
+		$('#logEmail').val(localStorage.usrname);
+		$('#logPass').val(localStorage.pass);
+	} else {
+		$('#remember_me').removeAttr('checked');
+		$('#logEmail').val('');
+		$('#logPass').val('');
+	}
+
 }
-	
